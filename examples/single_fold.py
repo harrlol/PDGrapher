9019bfdb1edaf467183f85306383c39dd23e26d3
Harry Li <87460010+harrlol@users.noreply.github.com>
2025-03-08 15:52:55 -0500

    they were right im stooped :)

--- examples/single_fold.py
+++ examples/single_fold.py
@@ -1,7 +1,7 @@
 import torch
 import torch.optim as optim
 
-from PDGrapher import Dataset, PDGrapher, Trainer
+from pdgrapher import Dataset, PDGrapher, Trainer
 
 import os
 torch.set_num_threads(5)
